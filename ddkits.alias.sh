dcd3a85f977ab9b9a7b172cb7d083e1b7a2ce82c
Sam Elayyoub <m22386@live.com>
2021-06-25 13:43:15 -0700

    Update ddkits.alias.sh

--- ddkits.alias.sh
+++ ddkits.alias.sh
@@ -114,7 +114,7 @@ ddk() {
       -keyout ddkits.site.key \
       -new \
       -out ddkits.site.crt \
-      -subj /CN=ddkits.site \
+      -subj "/C=CA/ST=QC/O=DDKits Inc/CN=ddkits.site" \
       -sha256 \
       -days 60
     mkdir ~/.ddkits/ddkits-files/ddkits/ssl
@@ -255,7 +255,7 @@ ddk() {
       -keyout $DDKITSSITES.key \
       -new \
       -out $DDKITSSITES.crt \
-      -subj /CN=$DDKITSSITES \
+      -subj "/C=CA/ST=QC/O=DDKits Inc/CN="$$DDKITSSITES"" \
       -sha256 \
       -days 60
     mv "$DDKITSSITES".key "$DDKITSFL"/ddkits-files/ddkits/ssl/
