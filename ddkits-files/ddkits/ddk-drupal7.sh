b6e1adde0d0807f454ebe7b4a00a631392264639
Sam Elayyoub <m22386@live.com>
2021-08-26 12:16:43 -0700

    Update ddk-drupal7.sh

--- ddkits-files/ddkits/ddk-drupal7.sh
+++ ddkits-files/ddkits/ddk-drupal7.sh
@@ -196,10 +196,11 @@ alias ddkd-$DDKITSSITES='docker exec -it ${DDKITSHOSTNAME}_ddkits_web '$WEBROOT'
 # create get into ddkits container
 echo $SUDOPASS | sudo -S cat ~/.ddkits_alias >/dev/null
 alias ddkc-$DDKITSSITES='docker exec -it '$DDKITSHOSTNAME'_ddkits_web /bin/bash'
-alias ddkd-$DDKITSSITES='docker exec -it '$DDKITSHOSTNAME'_ddkits_web /bin/bash -c "cd '$WEBROOT' && drush"'
+alias ddkd-$DDKITSSITES='docker exec -i '$DDKITSHOSTNAME'_ddkits_web vendor/bin/drush' >> ~/.ddkits_alias_web
 
 #  fixed the alias for machine
-echo "alias ddkc-"$DDKITSSITES"='ddk go && docker exec -it "$DDKITSHOSTNAME"_ddkits_web /bin/bash'" >>~/.ddkits_alias_web
+echo "alias ddkc-$DDKITSSITES='docker exec -it '$DDKITSHOSTNAME'_ddkits_web /bin/bash'" >> ~/.ddkits_alias_web
+echo "alias ddkd-"$DDKITSSITES"='ddk go && docker exec -it "$DDKITSHOSTNAME"_ddkits_web " >> ~/.ddkits_alias_web
 # echo "alias ddkd-"$DDKITSSITES"='docker exec -it "$DDKITSHOSTNAME"_ddkits_web /bin/bash -c 'cd $WEBROOT && drush''" >> ~/.ddkits_alias_web
 echo $SUDOPASS | sudo -S chmod -R 777 $DDKITSFL/drupal-deploy
 
